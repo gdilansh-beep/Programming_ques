d335debb7dbc09b7d1edd15da889817c02405ab3
Dilansh Gupta <gdilansh@gmail.com>
2026-07-10 10:17:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = amount + 1;
+
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    if (dp[i - coins[j]] + 1 < dp[i]) {
+                        dp[i] = dp[i + coins[j]] + 1;
+                    }
+                }
+            }
+        }
+
+        if (dp[amount] > amount)
+            return -1;
+
+        return dp[amount];
+    }
+}
